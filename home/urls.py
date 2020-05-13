7f7f8fd3e13e5d4a22fe9d7926e9eeb350df6524
inbalaltan <inbalaltan@gmail.com>
2020-05-13 08:23:14 -0700

    add DB buttn

--- home/urls.py
+++ home/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 				path('',views.home, name="home-page"),
 				path('<int:pk>/update/', myissuesUpdate.as_view(), name="my-issues-update"),
 				path('myissues/',views.myissues, name="my-issues"),
+				path('admin/',views.myissues, name="admin-db"),
 				
 			      ]
