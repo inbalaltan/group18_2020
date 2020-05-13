0d39ed5dc3b5cc656e40faaf6a912b8cf476f152
yblanke <yevb91@gmail.com>
2020-05-13 08:09:30 -0700

    minor visual bug fixes

--- home/views.py
+++ home/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Post
 from mifga.models import Mifga
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
-)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     var = {'posts': Post.objects.all().order_by('-date_posted')}
@@ -22,27 +17,32 @@ def contact(request):
 def help(request):
     return render(request, 'home/help.html') 
 
-
+@login_required
 def open_reports(request):
-    var = {'mifgas': Mifga.objects.filter(status="open").order_by('-date_posted')}
-    if(request.POST.get('assign')):
-        id = request.POST.get("id")
-        take_obs(request,id)
-    return render(request, 'home/open_reports.html',var)
+    if request.user.is_staff:
+        var = {'mifgas': Mifga.objects.filter(status="open").order_by('-date_posted')}
+        if(request.POST.get('assign')):
+            id = request.POST.get("id")
+            take_obs(request,id)
+        return render(request, 'home/open_reports.html',var)
+    raise Http404
 
-class myissuesUpdate(UpdateView):
+class myissuesUpdate(LoginRequiredMixin, UpdateView):
     model = Mifga
     template_name = 'home/myissues_form.html'
     fields = ['comment']
     def form_valid(self, form):
         return super().form_valid(form)
 
+@login_required
 def myissues(request):
-    var = {'mifgas': Mifga.objects.filter(agent_att = request.user).order_by("-status")}
-    if(request.POST.get('stat')):
-        id = request.POST.get("id")
-        close_issue(request,id)
-    return render(request, 'home/myissues.html',var)
+    if request.user.is_staff:
+        var = {'mifgas': Mifga.objects.filter(agent_att = request.user).order_by("-status")}
+        if(request.POST.get('stat')):
+            id = request.POST.get("id")
+            close_issue(request,id)
+        return render(request, 'home/myissues.html',var)
+    raise Http404
 
 def take_obs(request, obs_id):
     Mifga.objects.filter(id=obs_id).update(status = "in progress", agent_att = request.user)
@@ -53,4 +53,3 @@ def close_issue(request, obs_id):
     Mifga.objects.filter(id=obs_id).update(status = "closed")
     messages.success(request, f'issue closed successfully')
     return redirect('my-issues')
-
