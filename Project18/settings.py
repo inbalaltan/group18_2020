a024f906a01f963eb1fa4957d6c2a55c9530c21d
גלינה רזניק <galinareznikk@gmail.com>
2020-05-27 20:33:38 +0300

    PM2020T18-118

--- Project18/settings.py
+++ Project18/settings.py
@@ -128,3 +128,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home-page'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'team18projectdjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'danielby13'
